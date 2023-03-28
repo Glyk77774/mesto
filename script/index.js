9cfd6d64992c83f4b00a01428a193c0155ca21a4
Glyk <glyk77774@gmail.com>
2023-03-28 15:26:58 +0700

    На проверку 3

--- script/index.js
+++ script/index.js
@@ -7,30 +7,25 @@ const userNameInput = document.querySelector(".popup__input_type_name");
 const jobInput = document.querySelector(".popup__input_type_job");
 const popupEditForm = editProfile.querySelector(".popup__form");
 
-function editOpenForm() {
-  userNameInput.value = userName.textContent;
-  jobInput.value = jobName.textContent;
-};
-
-infoButton.addEventListener ('click', () => {
-  editProfile.classList.add('popup_opened');
-})
-
-editProfileClose.addEventListener ('click', () => {
-  editProfile.classList.remove('popup_opened');
-})
-
 function closePopup() {
   editProfile.classList.remove('popup_opened');
 }
+
+infoButton.addEventListener ('click', () => {
+  editProfile.classList.add('popup_opened');
+  userNameInput.value = userName.textContent;
+  jobInput.value = jobName.textContent;
+})
+
+editProfileClose.addEventListener ('click', () => {
+  closePopup();
+})
 
 function submitHandlerEdit(event) {
   event.preventDefault();
   userName.textContent = userNameInput.value;
   jobName.textContent = jobInput.value;
-  /*editProfile.classList.remove('popup_opened');*/
   closePopup();
 };
 
 popupEditForm.addEventListener("submit", submitHandlerEdit);
-infoButton.addEventListener("click", editOpenForm);
